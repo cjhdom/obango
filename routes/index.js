02a20d0b3728ac4dc9e567f44b40431381fdf888
cjhdom <ferus_gm@hanmail.net>
2016-12-07 22:27:07 +0900

    1차완성

--- routes/index.js
+++ routes/index.js
@@ -56,7 +56,7 @@ router.post('/coupon', (req, res, next) => {
   var longitude = Number(req.body.longitude);
   var latitude = Number(req.body.latitude);
   var desc = req.body.desc;
-  var count = req.body.count;
+  var count = Number(req.body.count);
   var loc = {
     longitude,
     latitude
@@ -75,7 +75,8 @@ router.post('/coupon', (req, res, next) => {
       var coupon = {
         desc,
         count,
-        loc
+        loc,
+        isUse: false
       };
 
       col.insertOne(coupon, function (err, r) {
@@ -84,7 +85,9 @@ router.post('/coupon', (req, res, next) => {
         } else {
           res.json({
             ok: 1,
-            id: r.insertedId
+            id: r.insertedId,
+            count: count,
+            isUse: false
           });
         }
         db.close();
@@ -121,6 +124,77 @@ router.delete('/coupon', (req, res, next) => {
   });
 });
 
+router.post('/coupon/enter', (req, res, next) => {
+  var id = req.body.id;
+
+  MongoClient.connect(url, function(err, db) {
+    if (err) {
+      res.send(JSON.stringify(err));
+    } else {
+      var col = db.collection('coupon');
+      var query = {
+        _id: ObjectId(id),
+        count: {$gt: 0},
+        isUse: false
+      };
+
+      var updateQuery = {
+        $set: {
+          isUse: true
+        }
+      };
+
+      col.findOneAndUpdate(query, updateQuery)
+        .then(result => {
+          return res.json(result);
+        })
+        .catch(result => {
+          console.log(JSON.stringify(result));
+          return res.json(result);
+        });
+    }
+  });
+});
+
+router.post('/coupon/exit/:result', (req, res, next) => {
+  var id = req.body.id;
+  var result = req.params.result;
+
+  MongoClient.connect(url, function(err, db) {
+    if (err) {
+      res.send(JSON.stringify(err));
+    } else {
+      var col = db.collection('coupon');
+      var query = {
+        _id: ObjectId(id),
+        count: {$gt: 0},
+        isUse: true
+      };
+
+      var updateQuery = {
+        $set: {
+          isUse: false
+        }
+      };
+
+      if (result === 'success') {
+        updateQuery.$inc = {
+          count: -1
+        };
+      }
+
+      col.findOneAndUpdate(query, updateQuery)
+        .then(response => {
+          return res.json(response);
+        })
+        .catch(response => {
+          console.log(JSON.stringify(response));
+          return res.json(response);
+        });
+    }
+  });
+});
+
 router.get('/fetch/:type', (req, res, next) => {
   var longitude = null;
   var latitude = null;
@@ -134,8 +208,6 @@ router.get('/fetch/:type', (req, res, next) => {
     res.json({ok:0});
   }
 
-  console.log(longitude + ',' + latitude + ',' + type);
-
   MongoClient.connect(url, function(err, db) {
     if (err) {
       res.send(JSON.stringify(err));
@@ -148,10 +220,16 @@ router.get('/fetch/:type', (req, res, next) => {
         options.maxDistanc = 0.00005 / 6.3781;
         options.spherical = true;
         options.num = 20;
+        options.query = {
+          count: {$gt: 0},
+          isUse: false
+        };
       } else {
         options.maxDistance = 0.0005 / 6.3781;
         options.spherical = true;
       }
+
+      console.log(JSON.stringify(options));
 
       col.geoNear(longitude, latitude, options, function (err, coupons) {
         if (err) {
@@ -181,7 +259,7 @@ router.get('/fetch/all', (req, res, next) => {
         db.close();
       })
     }
-  })
+  });
 });
 
 module.exports = router;
